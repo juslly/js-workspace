9f25b4136c2b3b98f5b8d69f77833fdcf4235050
System Administrator <root@jingdeMacBook-Pro.local>
2019-08-20 20:31:40 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// function createMultiplyTable (start, end) {
+//     return;
+// }
+// function checkInput(start,end){
+//     return;
+// }
+
+// function createTableWhitout(start,end){
+
+//     return start * end;
+// }
+
+// function formate(start,end,isCorrect){
+//     return;
+// }
+
+// module.exports = add;
+
+function add (left, right) {
+    return left + right;
+}
+
+module.exports = add;
